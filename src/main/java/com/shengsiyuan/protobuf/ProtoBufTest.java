8eba987445d675d3da61af77c523fe3670d15e75
yangmindi <1946334058@qq.com>
2020-01-31 20:01:16 +0800

    Protobuf instance

---
+++ src/main/java/com/shengsiyuan/protobuf/ProtoBufTest.java
@@ -0,0 +1,16 @@
+package com.shengsiyuan.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class ProtoBufTest {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        DataInfo.Student student = DataInfo.Student.newBuilder().
+                setName("张三").setAge(20).setAddress("北京").build();
+
+        byte[] student2ByteArray = student.toByteArray();
+
+        DataInfo.Student student2 = DataInfo.Student.parseFrom(student2ByteArray);
+
+        System.out.println(student2 );
+    }
+}
