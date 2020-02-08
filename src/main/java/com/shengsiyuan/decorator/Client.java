4cb083b784e7b424a7c5830d70423365b7f7894b
yangmindi <1946334058@qq.com>
2020-02-08 18:38:09 +0800

    装饰着模式

---
+++ src/main/java/com/shengsiyuan/decorator/Client.java
@@ -0,0 +1,8 @@
+package com.shengsiyuan.decorator;
+
+public class Client {
+    public static void main(String[] args) {
+        Component component = new ConcreteDecorator1(new ConcreteDecorator2(new ConcreteComponent()));
+        component.doSomething();
+    }
+}
