9918b5bd839e946c8a735ae43ecac5b8cdc2fe11
yangmindi <1946334058@qq.com>
2020-02-09 15:07:38 +0800

    NioiTest3

---
+++ src/main/java/com/shengsiyuan/nio/NioTest3.java
@@ -0,0 +1,27 @@
+package com.shengsiyuan.nio;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest3 {
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fileOutputStream = new FileOutputStream("NioTest3.txt");
+
+        byte[] message = "hyh ymd".getBytes();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        FileChannel channel = fileOutputStream.getChannel();
+
+        for (int i = 0; i < message.length; i++) {
+            byteBuffer.put(message[i]);
+        }
+
+        byteBuffer.flip();
+
+        channel.write(byteBuffer);
+
+        fileOutputStream.close();
+    }
+}
